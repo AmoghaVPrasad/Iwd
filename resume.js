c98fbe9ebf3f1d4d0c12e2045115f7dc25e22954
AmoghaVPrasad <155794474+AmoghaVPrasad@users.noreply.github.com>
2024-01-06 12:11:24 +0530

    Update resume.js

--- resume.js
+++ resume.js
@@ -16,3 +16,41 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+function check(form){
+
+	if (form.username.value==""){
+		alert("Username cannot be empty!")
+		return false;
+	}
+
+	if (form.emailID.value==""){
+    	alert("Email cannot be empty!")
+		return false;
+	}
+    else if (form.emailID.value.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z]+)*$/)==null){
+         alert("Invalid Email Address entered!")
+		return false;
+    }
+
+     if (form.mobilephone.value==""){
+    	alert("Mobile Number cannot be empty!")
+    	return false;
+    }
+    else if (form.mobilephone.value.match(/^(?!0+$)(\+\d{1,3}[- ]?)?(?!0+$)\d{10}$/)==null){
+    	alert("Invalid Mobile Number entered!")
+    	return false;
+    }
+    if(form.altmobilephone.value==""){
+			alert("Please enter an Alternate Number! If same as Mobile Number, select the checkbox.")
+			return false;
+		}
+	else if (form.altmobilephone.value.match(/^(?!0+$)(\+\d{1,3}[- ]?)?(?!0+$)\d{10}$/)==null){
+    	alert("Invalid Alternate Mobile Number entered!")
+    	return false;
+    }
+   
+ 
+
+   return true;
+
+}
